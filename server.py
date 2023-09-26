36f5b6bc48c925887fc5f9a7e9b64f57226d88e5
lvguanjun <1193965453@qq.com>
2023-09-26 12:23:13 +0800

    update some code
    Signed-off-by: lvguanjun <1193965453@qq.com>

--- server.py
+++ server.py
@@ -13,9 +13,11 @@ app = Flask(__name__)
 
 def require_token(fun):
     def wrapper(*args, **kwargs):
-        if not my_server["token"]:
-            return fun(*args, **kwargs)
-        if request.headers.get("Authorization", "") != "token " + my_server["token"]:
+        if (
+            my_server["token"]
+            and request.headers.get("Authorization", "")
+            != "token " + my_server["token"]
+        ):
             return jsonify({"message": "token is invalid"}), 401
         return fun(*args, **kwargs)
 
@@ -25,15 +27,16 @@ def require_token(fun):
 @app.route(GET_TOKEN_ROUTE, methods=["GET"])
 @require_token
 def get_token():
-    if token := get_token_from_redis():
-        return jsonify(token)
-    headers = {"Authorization": "token " + COCO_TOKEN}
-    response = requests.get(COCO_API_URL + GET_TOKEN_ROUTE, headers=headers)
-    if response.status_code == 200:
-        token = response.json()
-        cache_redis.set("token", json.dumps(token))
-        return jsonify(token)
-    return response.text, response.status_code
+    token = get_token_from_redis()
+    if not token:
+        headers = {"Authorization": "token " + COCO_TOKEN}
+        response = requests.get(COCO_API_URL + GET_TOKEN_ROUTE, headers=headers)
+        if response.status_code == 200:
+            token = response.json()
+            cache_redis.set("token", json.dumps(token))
+        else:
+            return response.json, response.status_code
+    return jsonify(token)
 
 
 if __name__ == "__main__":
--- utils/utils.py
+++ utils/utils.py
@@ -3,11 +3,17 @@ import time
 
 from config import cache_redis_client as cache_redis
 
+TOKEN_CACHE_KEY = "token"
+TOKEN_EXPIRATION_TIME = 60
 
 def get_token_from_redis():
-    cache_token = cache_redis.get("token")
-    if cache_token:
-        cache_token = json.loads(cache_token)
-        if cache_token["expires_at"] > int(time.time()) + 60:
-            return cache_token
+    try:
+        cache_token = cache_redis.get(TOKEN_CACHE_KEY)
+        if cache_token:
+            cache_token = json.loads(cache_token)
+            if cache_token["expires_at"] > int(time.time()) + TOKEN_EXPIRATION_TIME:
+                return cache_token
+    except Exception as e:
+        print(f"ERROR: get token from redis failed, {e=}")
+        pass
     return None
